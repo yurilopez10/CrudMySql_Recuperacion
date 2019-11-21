f049138c53951b902303125b5c0c0c4eca88cd3c
Yurilopez10 <alexan2000hotmal@gmail.com>
2019-11-21 15:39:06 -0600

    Subiendo crud con MySql de recuperación

--- app/src/main/java/com/example/crud_mysql/Consulta_RecyclerView.java
+++ app/src/main/java/com/example/crud_mysql/Consulta_RecyclerView.java
@@ -43,5 +43,28 @@ public class Consulta_RecyclerView extends AppCompatActivity {
 
     AlertDialog.Builder dialogo;
 
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            new android.app.AlertDialog.Builder(this)
+                    .setIcon(R.drawable.ic_close)
+                    .setTitle("Advertencia")
+                    .setMessage("¿Realmente desea salir?")
+                    .setNegativeButton(android.R.string.cancel, null)
+                    .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {//un listener que al pulsar, cierre la aplicacion
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .show();
+            return true;
+        }
+        //para las demas cosas, se reenvia el evento al listener habitual
+        return super.onKeyDown(keyCode, event);
+    }
+
+
+
 }
 
