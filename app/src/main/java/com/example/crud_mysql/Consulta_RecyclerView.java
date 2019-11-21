221a17199bc96576c4529ba59d74fc74c7eaf4b5
Yurilopez10 <alexan2000hotmal@gmail.com>
2019-11-21 15:40:13 -0600

    Subiendo crud con MySql de recuperación

--- app/src/main/java/com/example/crud_mysql/Consulta_RecyclerView.java
+++ app/src/main/java/com/example/crud_mysql/Consulta_RecyclerView.java
@@ -145,5 +145,30 @@ public class Consulta_RecyclerView extends AppCompatActivity {
     }
 
 
+    private void DialogConfirmacion(){
+        //startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        String mensaje = "¿Realmente desea salir?";
+        dialogo = new AlertDialog.Builder(Consulta_RecyclerView.this);
+        dialogo.setIcon(R.drawable.ic_close);
+        dialogo.setTitle("Advertencia");
+        dialogo.setMessage(mensaje);
+        dialogo.setCancelable(false);
+        dialogo.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogo, int id) {
+                /*Intent intent = new Intent(DashboardLuces.this, luces_control_sms.class);
+                startActivity(intent);*/
+                //MainActivity.this.finishAffinity();
+                finish();
+            }
+        });
+        dialogo.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialogo, int id) {
+                Toast.makeText(getApplicationContext(), "Operación Cancelada.", Toast.LENGTH_LONG).show();
+            }
+        });
+        dialogo.show();
+    }
+
+
 }
 
