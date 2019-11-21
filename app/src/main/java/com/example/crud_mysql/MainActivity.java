e18e51b23699ef9c504ecabf1ff2c908f540b129
Yurilopez10 <alexan2000hotmal@gmail.com>
2019-11-21 16:39:45 -0600

    Subiendo

--- app/src/main/java/com/example/crud_mysql/MainActivity.java
+++ app/src/main/java/com/example/crud_mysql/MainActivity.java
@@ -314,7 +314,7 @@ public class MainActivity extends AppCompatActivity {
             alertDialogBuilder.setTitle("Proyecto creado por:");
             // Establecer mensaje de diálogo
             alertDialogBuilder
-                    .setMessage("Yuri  \nSIS 21B")
+                    .setMessage("Yuri López \nSIS 22")
                     .setCancelable(false)
                     .setNegativeButton("Aceptar", new DialogInterface.OnClickListener() {
                         public void onClick(DialogInterface dialog, int id) {
